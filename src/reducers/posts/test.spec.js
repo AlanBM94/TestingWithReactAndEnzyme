e6dacb848126245733291f47b42b46c1a3ddbc11
alanbm94 <sevilbm94@hotmail.com>
2020-07-20 12:56:36 -0500

    Tests Posts reducer

---
+++ src/reducers/posts/test.spec.js
@@ -0,0 +1,24 @@
+import { types } from "./../../actions/types";
+import postsReducer from "./reducer";
+
+describe("Post Reducer", () => {
+  it("Should return default state", () => {
+    const newState = postsReducer(undefined, {});
+    expect(newState).toEqual([]);
+  });
+
+  it("Should return newState if receiving a type", () => {
+    const posts = [
+      { title: "Test 1" },
+      { title: "Test 1" },
+      { title: "Test 1" },
+    ];
+
+    const newState = postsReducer(undefined, {
+      type: types.GET_POSTS,
+      payload: posts,
+    });
+
+    expect(newState).toEqual(posts);
+  });
+});
